09b805a06bb858a7219a9bd5c8ac26d449ca5921
dmptmkn <dm.ptmkn@yandex.ru>
2026-05-11 22:27:24 +0300

    feat: rest api and dtos

--- customer-service/src/main/java/com/example/infrastructure/web/dto/RegisterCustomerDto.java
+++ customer-service/src/main/java/com/example/infrastructure/web/dto/RegisterCustomerDto.java
@@ -1,24 +1,14 @@
 package com.example.infrastructure.web.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record RegisterCustomerDto(
-        @NotBlank @Email
-        String email,
-        @NotBlank
-        String phone,
-        @NotBlank
-        String name,
-        @NotBlank
-        String country,
-        @NotBlank
-        String zipcode,
-        @NotBlank
-        String city,
-        @NotBlank
-        String street,
-        @NotBlank
-        String building,
-        String apartment
-) {}
+public record RegisterCustomerDto(String email,
+                                  String phone,
+                                  String userName,
+                                  String firstName,
+                                  String lastName,
+                                  String country,
+                                  String zipcode,
+                                  String city,
+                                  String street,
+                                  String building,
+                                  String apartment) {
+}
